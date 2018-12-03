31a4a59242a241643c9cf3947d64c1acbf3e7268
Diazhao <mrzhaoh@126.com>
2018-12-03 20:48:14 +0800

    增加一些数据

---
+++ vuepreview-master/build/webpack.config.prod.js
@@ -0,0 +1,7 @@
+const merge =  require('webpack-merge');
+const baseConfig = require('./webpack.config.base');
+const webpack = require('webpack');
+const path = require('path')
+module.exports = merge(baseConfig,{
+    plugins: []
+})
